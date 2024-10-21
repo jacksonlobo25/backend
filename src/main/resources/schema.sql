d03115b4e5c328f3b562f9f492efe6bfd7f2ff16
Jackson Lobo <jacksonlobo2001@gmail.com>
2024-10-21 12:48:11 +0530

    Added schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password';
+FLUSH PRIVILEGES;
+
+CREATE USER 'user'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON mydatabase.* TO 'springuser'@'%';
+FLUSH PRIVILEGES;
